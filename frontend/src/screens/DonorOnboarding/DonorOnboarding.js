0a734ab4fbc638ba9405f0be999d5dac78227eca
ProBeta1 <humblebeta@gmail.com>
2020-10-24 13:32:31 +0530

    structure added

---
+++ frontend/src/screens/DonorOnboarding/DonorOnboarding.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import styles from './styles';
+import { firebase } from '../../firebase/config'
+import { View, Text } from 'react-native';
+
+export default function DonorOnboarding(props) {
+
+    return(
+        <View>
+          <Text>hello don</Text>
+        </View>
+    )
+}
---
+++ frontend/src/screens/RecOnboarding/RecOnboarding.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import styles from './styles';
+import { firebase } from '../../firebase/config'
+import { View,Text } from 'react-native';
+
+export default function RecOnboarding(props) {
+
+    return(
+        <View>
+          <Text>hey rec</Text>
+        </View>
+    )
+}
