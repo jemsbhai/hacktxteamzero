01a6e299d5476caf9c80617ae0d05a913e36a9ea
ProBeta1 <humblebeta@gmail.com>
2020-10-24 15:50:36 +0530

    yeah yeah yeah yeah onbdg scrn rec

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -10,7 +10,7 @@ export default function HomeScreen(props) {
 
     if(type === "Donor"){
         return(
-            <DonorOnboarding />
+            <DonorOnboarding {...props}/>
         )
     }
 
