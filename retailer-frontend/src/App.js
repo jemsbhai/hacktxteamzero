7194288e43ef7861a4bbd570afe1644aae46bf9d
Darren <darrenzhang200027@gmail.com>
2020-10-24 17:55:38 -0400

    added home page

--- retailer-frontend/src/App.js
+++ retailer-frontend/src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "materialize-css/dist/css/materialize.min.css";
+import React, { Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/home";
+
 
 function App() {
+  // const dispatch = useDispatch();
+  // const [display, setDisplay] = React.useState("block");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="navbar-fixed navbar-top">
+        <nav className="white">
+          <div class="nav-wrapper">
+            <Link to="/" class="brand-logo left">
+              {/* <img src={Logo}></img> */}
+            </Link>
+            <ul class="right">
+              <li>
+                <Link
+                  to="/logreg/login"
+                  class="waves-effect waves-light btn btn-base btn-small-radius"
+                >
+                  Login/ Register
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <div>
+        {/* <Header /> */}
+        <Route exact path='/' component={Home}/>
+        {/* <Route exact path='/' component={Landing} />
+      <Route exact path='/register' component={Register} /> */}
+        {/* <Bottom /> */}
+        {/* <Route exact path="/" component={Landing} /> */}
+
+      </div>
+
+      <div className="navbar-fixed navbar-bottom navv">
+        <nav className="black">
+          <div class="nav-wrapper">
+            <ul class="center">
+              <li>
+                <a
+                  href="#"
+                  target="_blank"
+                >
+                  Site map
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  target="_blank"
+                >
+                  Terms
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  target="_blank"
+                >
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  target="_blank"
+                >
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </Fragment>
   );
 }
 
--- retailer-frontend/src/index.js
+++ retailer-frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
 
