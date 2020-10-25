0c0d1dacbd2a913af1af03ef82e5ad246fafa3c3
ProBeta1 <humblebeta@gmail.com>
2020-10-25 09:39:08 +0530

    123

--- frontend/src/screens/DonorOnboarding/DonorOnboarding.js
+++ frontend/src/screens/DonorOnboarding/DonorOnboarding.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react'
 import styles from './styles';
 import { firebase } from '../../firebase/config'
 import { View,Text } from 'react-native';
+import Botnav from '../Botnav';
 
 export default function DonorOnboarding(props) {
 
+  // testing purpose only , needs to be moved to rec side
     return(
-        <View>
-          <Text>hey rec</Text>
-        </View>
+      <View style={styles.container}>
+        <Botnav/>
+      </View>
     )
 }
